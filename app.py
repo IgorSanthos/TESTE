fa75d63570364c1e0fb2fc415b5e8cc6f536f4d3
Igor Santhos <igorsanthoss@gmail.com>
2024-06-19 16:26:14 -0300

    add-move

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask  import Flask, render_template
+from flask  import Flask, render_template,redirect,url_for
 app = Flask(__name__)
 @app.route('/')
 def index():        
     return render_template('index.html')
+
+@app.route('/move', methods=['POST'])
+def move():
+       return redirect(url_for('index'))
 
 if __name__ == "__main__":              
     app.run(debug=True)
